4de15938b7d95ededa594a9d03032061289a374f
alex nichols <alex nichols>
2017-04-17 22:55:05 -0700

    server and routes setup

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var friendsArray = [
+	{	
+		"name": "Alex",
+		"photo": "https://s-media-cache-ak0.pinimg.com/736x/24/99/03/249903173ee16b3346ba320a24e56a8b.jpg",
+		"scores": [
+			3,
+			4,
+			1,
+			5,
+			3,
+			1,
+			3,
+			2,
+			4,
+			4
+		]
+
+	}
+]
+
+module.exports = friendsArray;
