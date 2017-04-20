92df86787fbdc05a4c66da5863c9efca9a6a0d8c
alex nichols <alex nichols>
2017-04-19 22:37:34 -0700

    separating survey logic

---
+++ app/public/surveyLogic.js
@@ -0,0 +1,37 @@
+$("#formSubmit").on("click", function(){
+
+	var formValidation = function(){
+
+		var validation = true;
+		if($("#name").val() === ""){
+			validation = false;
+		};
+
+		if($("#photo").val() === ""){
+			validation = false;
+		};
+
+		$(".chosen-select").each(function(){
+			if ($(this).val() === ""){
+				validation = false;
+			};
+		});
+
+		return validation;
+	};
+
+	if(formValidation == true){
+
+		var newUser = {
+			name: $("#name").val().trim(),
+			photo: $("#photo").val().trim(),
+			scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]
+		};
+
+		var currentURL = window.location.origin;
+
+		$.post(currentURL + "/api/friends/", newUser, function(data){
+
+		});
+	};
+});
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friendsArray = require("/../data/friends.js");
+var friendsArray = require("../data/friends.js");
 
 
 module.exports = function(app){
@@ -8,5 +8,14 @@ module.exports = function(app){
 
 	app.post("/api/friends", function(req, res){
 		friendsArray.push(req.body);
-	})
-}
+		res.json(friendsArray);
+
+		for(var i=0; i < friendsArray.length; i++){
+			var eachScore = friendsArray[i].scores;
+			var friendScore = eachScore.map(function(num){
+				var eachNum = parseInt(num, 10);
+				console.log(eachNum);
+			});
+		};
+	});
+};
